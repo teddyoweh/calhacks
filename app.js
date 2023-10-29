8b550cd491c4197087438c4b3a101972e22d2cdd
Kevin Li <jshiansongli@gmail.com>
2023-10-29 10:11:25 -0700

    suffix change

--- app.js
+++ app.js
@@ -151,8 +151,9 @@ app.post("/api/create", upload.single("model"), async (req, res) => {
         console.log(formattedMovesetString)
 
         console.log(cardData.toString)
-        let suffix = cardData.toString().length;
-        newCardName = `${name}_${suffix}`
+        
+        let suffix = Object.keys(cardData).length;
+		    newCardName = `${name}_${suffix}`;
 
         const newCard = {
             cardName: name,
