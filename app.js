56c64221313db7f730dafd03a901fbeb5045001b
Kevin Li <kevin@Kevins-MacBook-Pro.local>
2023-10-28 20:10:56 -0700

    mongoose

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 
 const app = express();
 const PORT = 3000;
 
 
 
+
 app.listen(PORT, () => {
-	console.log($`Server started on port ${PORT}`);
+	console.log(`Server started on port ${PORT}`);
 });
